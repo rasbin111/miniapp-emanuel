a789f2c048888a2bc13b19c985ee3aae2b78d3f8
rgt <rasbingodarthapa@gmail.com>
2025-11-06 18:47:21 +0545

    header update

--- frontend/src/components/headerWithBackground/index.jsx
+++ frontend/src/components/headerWithBackground/index.jsx
@@ -25,73 +25,84 @@ const HeaderWithBackground = () => {
       <img src="bg.jpg" alt="" className="background-img" />
       <nav className="navigation-container">
         <header className="navigation-header">
-          <GiHamburgerMenu
-            className="burger-menu"
-            onClick={() => setShowMenuItems((prev) => !prev)}
-          />
-          <img src="diamond.png" alt="" className="logo" />
-          <div
-            className={
-              showMenuItems ? `right-section-nav nav-list` : `right-section-nav`
-            }
-          >
-            <a href="/home" className="navigation-item">
-              {" "}
-              {menuItems.home}{" "}
-            </a>
-            <a href="/order" className="navigation-item">
-              {" "}
-              {menuItems.order}{" "}
-            </a>
-            <a href="/customers" className="navigation-item">
-              {" "}
-              {menuItems.our_customers}{" "}
-            </a>
-            <a href="/about" className="navigation-item">
-              {" "}
-              {menuItems.about_us}{" "}
-            </a>
-            <a href="/contact-us" className="navigation-item">
-              {" "}
-              {menuItems.contact_us}{" "}
-            </a>
-          </div>
-          <span className="lang-selector-container">
-            <div
-              onClick={() => setShowLangSelector((prev) => !prev)}
-              className="lang-selector lang-selector-item"
-            >
-              {language === "eng" ? "English" : "Svenska"}
-              <img
-                src={language === "eng" ? "eng.png" : "se.png"}
-                className="lang-selector-img"
-              />
+          <div className="navigation-section">
+            <GiHamburgerMenu
+              className="burger-menu"
+              onClick={() => setShowMenuItems((prev) => !prev)}
+            />
+            <div className="logo-box">
+              <a href="/">
+                <img src="diamond.png" alt="" className="logo" />
+              </a>
             </div>
-            {showLangSelector && (
-              <div className="language-dd-list">
-                <div
-                  className="lang-selector-item"
-                  onClick={() => {
-                    setShowLangSelector(false);
-                    setLanguage("swe");
-                  }}
-                >
+            <div className="navigation-menu-bar">
+              <div
+                className={
+                  showMenuItems
+                    ? `right-section-nav nav-list`
+                    : `right-section-nav`
+                }
+              >
+                <a href="/home" className="navigation-item">
                   {" "}
-                  Svenska <img src="se.png" className="lang-selector-img" />
-                </div>
-                <div
-                  className="lang-selector-item"
-                  onClick={() => {
-                    setShowLangSelector(false);
-                    setLanguage("eng");
-                  }}
-                >
+                  {menuItems.home}{" "}
+                </a>
+                <a href="/order" className="navigation-item">
                   {" "}
-                  English <img src="eng.png" className="lang-selector-img" />
-                </div>
+                  {menuItems.order}{" "}
+                </a>
+                <a href="/customers" className="navigation-item">
+                  {" "}
+                  {menuItems.our_customers}{" "}
+                </a>
+                <a href="/about" className="navigation-item">
+                  {" "}
+                  {menuItems.about_us}{" "}
+                </a>
+                <a href="/contact-us" className="navigation-item">
+                  {" "}
+                  {menuItems.contact_us}{" "}
+                </a>
               </div>
-            )}
-          </span>
+              <span className="lang-selector-container">
+                <div
+                  onClick={() => setShowLangSelector((prev) => !prev)}
+                  className="lang-selector lang-selector-item"
+                >
+                  {language === "eng" ? "English" : "Svenska"}
+                  <img
+                    src={language === "eng" ? "eng.png" : "se.png"}
+                    className="lang-selector-img"
+                  />
+                </div>
+                {showLangSelector && (
+                  <div className="language-dd-list">
+                    <div
+                      className="lang-selector-item"
+                      onClick={() => {
+                        setShowLangSelector(false);
+                        setLanguage("swe");
+                      }}
+                    >
+                      {" "}
+                      Svenska <img src="se.png" className="lang-selector-img" />
+                    </div>
+                    <div
+                      className="lang-selector-item"
+                      onClick={() => {
+                        setShowLangSelector(false);
+                        setLanguage("eng");
+                      }}
+                    >
+                      {" "}
+                      English{" "}
+                      <img src="eng.png" className="lang-selector-img" />
+                    </div>
+                  </div>
+                )}
+              </span>
+            </div>
+          </div>
         </header>
       </nav>
     </div>
